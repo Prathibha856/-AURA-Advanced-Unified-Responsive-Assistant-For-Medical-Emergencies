2de3289f40433e4460efbbc6192a06c5eb25bfdd
Rakshith-Achar-08 <rakshithachar08@gmail.com>
2026-08-06 21:40:27 +0530

    security configured

---
+++ backend/aura/src/main/java/com/project/aura/Configuration/Config.java
@@ -0,0 +1,40 @@
+package com.project.aura.Configuration;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configurers.userdetails.DaoAuthenticationConfigurer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+public class Config {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity){
+        httpSecurity.csrf(customize -> customize.disable())
+                .authorizeHttpRequests(request -> request
+                        .requestMatchers("/register",  "/login")
+                        .permitAll()
+                        .anyRequest().authenticated())
+                .formLogin(Customizer.withDefaults())
+                .httpBasic(Customizer.withDefaults());
+
+        return httpSecurity.build();
+    }
+
+    @Autowired
+    public UserDetailsService userDetailsService;
+
+    @Bean
+    public AuthenticationProvider authenticationProvider(){
+
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider(userDetailsService);
+        provider.setPasswordEncoder(new BCryptPasswordEncoder(3));
+//        provider.setUserDetailsService(userDetailsService);
+        return provider;
+    }
+}
