eb6d81344d89ea82f799f45a8ab1dd3c0134c7fd
Rakshith-Achar-08 <rakshithachar08@gmail.com>
2026-08-07 23:34:34 +0530

    done with security setup almost

--- backend/aura/src/main/java/com/project/aura/Controller/AuthController.java
+++ backend/aura/src/main/java/com/project/aura/Controller/AuthController.java
@@ -1,21 +1,26 @@
 package com.project.aura.Controller;
 
+import com.project.aura.DTO.RegisterRequest;
 import com.project.aura.Entity.Users;
 import com.project.aura.Service.RegisterService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/auth")
 public class AuthController {
 
     @Autowired
     private RegisterService registerService;
 
-    @PostMapping("/api/auth/register")
-    public Users newRegister(@RequestBody Users users){
-        return registerService.newRegister(users);
+    @PostMapping("/register")
+    public ResponseEntity<String> newRegister(@RequestBody RegisterRequest registerRequest){
+        Users saveUsers = registerService.newRegister(registerRequest);
+        return ResponseEntity.ok("User registered successfully with ID: " + saveUsers.getUserid());
     }
 
 
--- backend/aura/src/main/java/com/project/aura/DTO/RegisterRequest.java
+++ backend/aura/src/main/java/com/project/aura/DTO/RegisterRequest.java
@@ -4,6 +4,10 @@ import com.project.aura.Entity.Users;
 import lombok.Getter;
 import lombok.Setter;
 
+
+//this DTO is using for the user to register only by using username, etc etc
+//the user should not pass thier ID, it should be done automatically by DB
+
 @Getter
 @Setter
 public class RegisterRequest {
