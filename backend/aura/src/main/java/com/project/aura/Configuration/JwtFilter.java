edb23b578ac1c994ffa23d65162dfe3a2aa875da
Rakshith-Achar-08 <rakshithachar08@gmail.com>
2026-08-12 23:44:49 +0530

    added phone number

--- backend/aura/src/main/java/com/project/aura/Configuration/JwtFilter.java
+++ backend/aura/src/main/java/com/project/aura/Configuration/JwtFilter.java
@@ -1,52 +1,52 @@
-package com.project.aura.Configuration;
-
-import com.project.aura.Service.JWTService;
-import com.project.aura.Service.MyUsersDetailsService;
-import jakarta.servlet.FilterChain;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
-import org.springframework.web.context.annotation.ApplicationScope;
-import org.springframework.web.filter.OncePerRequestFilter;
-
-import java.io.IOException;
-
-public class JwtFilter extends OncePerRequestFilter {
-
-    @Autowired
-    private JWTService jwtService;
-
-    @Autowired
-    ApplicationContext context;
-
-    @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-//        next
-        String authHeader=request.getHeader("Authorization");
-        String token = null;
-        String username=null;
-
-        if(authHeader != null && authHeader.startsWith("Bearer ")){
-            token = authHeader.substring(7);
-            username = jwtService.extratUserName(token);
-        }
-
-        UserDetails userDetails = context.getBean(MyUsersDetailsService.class).loadUserByUsername(username);
-        if(username != null && SecurityContextHolder.getContext().getAuthentication()==null){
-            if(jwtService.validataToken(token, userDetails)){
-                UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken=
-                        new UsernamePasswordAuthenticationToken(userDetails,null,userDetails.getAuthorities());
-                usernamePasswordAuthenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-                SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
-            }
-        }
-        filterChain.doFilter(request,response);
-    }
-}
+//package com.project.aura.Configuration;
+//
+//import com.project.aura.Service.JWTService;
+//import com.project.aura.Service.MyUsersDetailsService;
+//import jakarta.servlet.FilterChain;
+//import jakarta.servlet.ServletException;
+//import jakarta.servlet.http.HttpServletRequest;
+//import jakarta.servlet.http.HttpServletResponse;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.context.ApplicationContext;
+//import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+//import org.springframework.security.core.context.SecurityContextHolder;
+//import org.springframework.security.core.userdetails.UserDetails;
+//import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+//import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+//import org.springframework.web.context.annotation.ApplicationScope;
+//import org.springframework.web.filter.OncePerRequestFilter;
+//
+//import java.io.IOException;
+//
+//public class JwtFilter extends OncePerRequestFilter {
+//
+//    @Autowired
+//    private JWTService jwtService;
+//
+//    @Autowired
+//    ApplicationContext context;
+//
+//    @Override
+//    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+////        next
+//        String authHeader=request.getHeader("Authorization");
+//        String token = null;
+//        String username=null;
+//
+//        if(authHeader != null && authHeader.startsWith("Bearer ")){
+//            token = authHeader.substring(7);
+//            username = jwtService.extratUserName(token);
+//        }
+//
+//        UserDetails userDetails = context.getBean(MyUsersDetailsService.class).loadUserByUsername(username);
+//        if(username != null && SecurityContextHolder.getContext().getAuthentication()==null){
+//            if(jwtService.validataToken(token, userDetails)){
+//                UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken=
+//                        new UsernamePasswordAuthenticationToken(userDetails,null,userDetails.getAuthorities());
+//                usernamePasswordAuthenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+//                SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
+//            }
+//        }
+//        filterChain.doFilter(request,response);
+//    }
+//}
